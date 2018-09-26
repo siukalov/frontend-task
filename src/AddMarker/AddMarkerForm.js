4f7f3fba7d9bc89775be3313518f4fdf7446b7a0
Anton Siukalov <unlizw@gmail.com>
2018-09-26 20:41:42 +0300

    refactor, tests: AddMarker
    - split AddMarker to connected and disconnected from Redux components
    - add tests for AddMarker

---
+++ src/AddMarker/AddMarkerForm.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import isUndefined from 'lodash/isUndefined';
+import {
+  Input, Form, FormInput, FormItem, Button, MarkerForm,
+} from './Styled';
+
+const fieldDecoratorRules = {
+  rules: [{ required: true, min: 1, message: 'Please input a marker name' }],
+};
+
+const hasErrors = fieldsError => Object.keys(fieldsError).some(field => fieldsError[field]);
+
+class AddMarker extends React.Component {
+  static propTypes = {
+    addMarker: PropTypes.func.isRequired,
+    form: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  };
+
+  componentDidMount() {
+    // disable the add button at the initial render
+    const { form } = this.props;
+    form.validateFields();
+  }
+
+  componentDidUpdate() {
+    // disable the add button for an empty input at the subsequent renders
+    const { form } = this.props;
+    const shouldValidate = isUndefined(form.getFieldValue('name')) && isUndefined(form.getFieldError('name'));
+
+    if (shouldValidate) {
+      form.validateFields();
+    }
+  }
+
+  handleSubmit = (e) => {
+    const {
+      addMarker,
+      form: {
+        getFieldValue,
+        resetFields,
+      },
+    } = this.props;
+
+    e.preventDefault();
+    const name = getFieldValue('name');
+    resetFields(['name']);
+    addMarker(name);
+  };
+
+  render() {
+    const { form } = this.props;
+
+    const {
+      getFieldDecorator,
+      getFieldsError,
+      getFieldError,
+      isFieldTouched,
+
+    } = form;
+
+    const fieldDecorator = getFieldDecorator('name', fieldDecoratorRules);
+    // don't show an error unless the field is touched
+    const nameError = isFieldTouched('name') && getFieldError('name');
+
+    return (
+      <MarkerForm
+        hideRequiredMark
+        onSubmit={this.handleSubmit}
+      >
+        <FormInput validateStatus={nameError ? 'error' : ''} help={nameError || ''}>
+          {fieldDecorator(<Input />)}
+        </FormInput>
+        <FormItem>
+          <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+            Add marker
+          </Button>
+        </FormItem>
+      </MarkerForm>
+    );
+  }
+}
+
+export default Form.create({})(AddMarker);
--- src/AddMarker/index.js
+++ src/AddMarker/index.js
@@ -1,75 +1,14 @@
-import React from 'react';
-import isUndefined from 'lodash/isUndefined';
 import { connect } from 'react-redux';
 import { addMarker } from '../actions';
-import {
-  Input, Form, FormInput, FormItem, Button, MarkerForm,
-} from './Styled';
+import AddMarkerForm from './AddMarkerForm';
 
-const fieldDecoratorRules = {
-  rules: [{ required: true, min: 1, message: 'Please input a marker name' }],
-};
+const mapDispatchToProps = dispatch => ({
+  addMarker: name => dispatch(addMarker(name)),
+});
 
-function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field]);
-}
+export { AddMarkerForm, mapDispatchToProps };
 
-class AddMarker extends React.Component {
-  componentDidMount() {
-    // To disabled the add button at the initial render
-    const { form } = this.props;
-    form.validateFields();
-  }
-
-  componentDidUpdate() {
-    // To disabled the add button at the subsequent renders
-    const { form } = this.props;
-    const shouldValidate = isUndefined(form.getFieldValue('name')) && isUndefined(form.getFieldError('name'));
-
-    if (shouldValidate) {
-      form.validateFields();
-    }
-  }
-
-  handleSubmit = (resetFields, getFieldValue, dispatch) => (e) => {
-    e.preventDefault();
-    const name = getFieldValue('name');
-    resetFields(['name']);
-    dispatch(addMarker(name));
-  };
-
-  render() {
-    const { dispatch, form } = this.props;
-
-    const {
-      getFieldDecorator,
-      getFieldValue,
-      getFieldsError,
-      getFieldError,
-      isFieldTouched,
-      resetFields,
-    } = form;
-
-    const fieldDecorator = getFieldDecorator('name', fieldDecoratorRules);
-    // don't show error unless a field is touched
-    const nameError = isFieldTouched('name') && getFieldError('name');
-
-    return (
-      <MarkerForm
-        hideRequiredMark
-        onSubmit={this.handleSubmit(resetFields, getFieldValue, dispatch)}
-      >
-        <FormInput validateStatus={nameError ? 'error' : ''} help={nameError || ''}>
-          {fieldDecorator(<Input />)}
-        </FormInput>
-        <FormItem>
-          <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
-            Add marker
-          </Button>
-        </FormItem>
-      </MarkerForm>
-    );
-  }
-}
-
-export default connect()(Form.create({})(AddMarker));
+export default connect(
+  null,
+  mapDispatchToProps,
+)(AddMarkerForm);
