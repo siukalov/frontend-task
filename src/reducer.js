319612f09654a302ea539ada3797809df565ac63
Anton Siukalov <unlizw@gmail.com>
2018-10-15 18:17:21 +0300

    refactor: reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,7 @@
 import Immutable from 'seamless-immutable';
 import reject from 'lodash/reject';
 import { arrayMove } from 'react-sortable-hoc';
-import {
-  ADD_MARKER,
-  REMOVE_MARKER,
-  REORDER_MARKERS,
-  SAVE_MAP_CENTER,
-  ADD_PLACEMARK,
-  UPDATE_MARKER_COORDS,
-} from './actionTypes';
+import * as actionTypes from './actionTypes';
 
 const initialState = Immutable({
   center: null,
@@ -25,7 +18,7 @@ const updateMarker = (action, field) => (marker) => {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ADD_MARKER:
+    case actionTypes.ADD_MARKER:
       return state.merge({
         markers: [
           ...state.markers,
@@ -37,23 +30,23 @@ export default (state = initialState, action) => {
           },
         ],
       });
-    case REMOVE_MARKER:
+    case actionTypes.REMOVE_MARKER:
       return state.merge({
         markers: reject(state.markers, { id: action.id }),
       });
-    case REORDER_MARKERS:
+    case actionTypes.REORDER_MARKERS:
       return state.merge({
         markers: arrayMove([...state.markers], action.oldIndex, action.newIndex),
       });
-    case SAVE_MAP_CENTER:
+    case actionTypes.SAVE_MAP_CENTER:
       return state.merge({
         center: action.center,
       });
-    case UPDATE_MARKER_COORDS:
+    case actionTypes.UPDATE_MARKER_COORDS:
       return state.merge({
         markers: state.markers.map(updateMarker(action, 'coordinates')),
       });
-    case ADD_PLACEMARK:
+    case actionTypes.ADD_PLACEMARK:
       return state.merge({
         markers: state.markers.map(updateMarker(action, 'onMap')),
       });
