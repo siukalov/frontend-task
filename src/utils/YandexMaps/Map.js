b1857f16063171a2271e30e0342f66bda3ad2464
Anton Siukalov <unlizw@gmail.com>
2018-10-15 19:26:22 +0300

    refactor: Map

--- src/utils/YandexMaps/Map.js
+++ src/utils/YandexMaps/Map.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Map extends Component {
-  // see https://github.com/facebook/react/issues/6653
   static defaultProps = {
     children: undefined,
-    ymaps: undefined,
   };
 
   static propTypes = {
     width: PropTypes.string.isRequired,
     height: PropTypes.string.isRequired,
     settings: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-    ymaps: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    ymaps: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
     saveMapInstance: PropTypes.func.isRequired,
     captureMapUpdate: PropTypes.func.isRequired,
     children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
